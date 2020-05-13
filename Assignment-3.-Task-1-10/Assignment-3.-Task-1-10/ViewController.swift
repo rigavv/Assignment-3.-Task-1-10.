7fe1fa553b298eb5796dc61042e01ce03958b8f9
Viktor Riga <rigavv@gmail.com>
2020-05-13 19:15:02 +0300

    Task 2. Create a line with your middle name and check it for the end of “ich / on”

--- Assignment-3.-Task-1-10/Assignment-3.-Task-1-10/ViewController.swift
+++ Assignment-3.-Task-1-10/Assignment-3.-Task-1-10/ViewController.swift
@@ -20,15 +20,34 @@ class ViewController: UIViewController {
         
 // -----
         
+// Task 2. Create a line with your middle name and check it for the end of “ich / on”
+        
+        checkMiddleName (middleName: "Валериевич")
+//        checkMiddleName (middleName: "Валериевна")
+        
+//-----
+        
+        
     } // End viewDidLoad
 
 
-// Task 1. Create a string with your name, print the number of characters contained in it. 
+// Task 1. Create a string with your name, print the number of characters contained in it.
 
     func printNameNumber (name:String) {
         print(name.count)
     }
 // -----
+    
+// Task 2. Create a line with your middle name and check it for the end of “ich / on”
+            
+    func checkMiddleName (middleName:String) {
+        let theEnd = middleName.suffix(2)
+        if middleName.hasSuffix("ич") || middleName.hasSuffix("на") {
+            print("В отчестве \(middleName), присутствует окончание \(theEnd), ")
+        }
+    }
+            
+//-----
 
 
 }
