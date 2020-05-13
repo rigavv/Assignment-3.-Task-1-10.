88386ce6f293e08d566db87affa30efcfd8bea7d
Viktor Riga <rigavv@gmail.com>
2020-05-13 23:18:50 +0300

    Task 3. Create a line where your First Name Last Name “IvanVasilevich". You need to split into two separate lines from the previous one to create a third, where they will both be separated by a space

--- Assignment-3.-Task-1-10/Assignment-3.-Task-1-10/ViewController.swift
+++ Assignment-3.-Task-1-10/Assignment-3.-Task-1-10/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
@@ -15,17 +16,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
 // Task 1. Create a string with your name, print the number of characters contained in it.
-
+        print("Task 1")
         printNameNumber (name: "Viktor")
-        
+        print("______")
 // -----
         
 // Task 2. Create a line with your middle name and check it for the end of “ich / on”
-        
-        checkMiddleName (middleName: "Валериевич")
-//        checkMiddleName (middleName: "Валериевна")
-        
+        print("Task 2")
+//        checkMiddleName (middleName: "Валериевич")
+        checkMiddleName (middleName: "Валериевна")
+        print("______")
 //-----
+        
+// Task 3. Create a line where your First Name Last Name “IvanVasilevich". You need to split into two separate lines from the previous one to create a third, where they will both be separated by a space
+        print("Task 3")
+        
+        separateFullName(fullName: "ViktorValerievich")
+        
+        print("______")
+//-----
+        
+        
         
         
     } // End viewDidLoad
@@ -42,12 +53,35 @@ class ViewController: UIViewController {
             
     func checkMiddleName (middleName:String) {
         let theEnd = middleName.suffix(2)
+        print(theEnd)
         if middleName.hasSuffix("ич") || middleName.hasSuffix("на") {
-            print("В отчестве \(middleName), присутствует окончание \(theEnd), ")
+            print("В отчестве \(middleName), присутствует окончание -  \(theEnd)")
         }
     }
             
 //-----
+    
+// Task 3. Create a line where your First Name Last Name “IvanVasilevich". You need to split into two separate lines from the previous one to create a third, where they will both be separated by a space
+                    
+    func separateFullName (fullName: String) {
+        print(fullName)
+        var name = ""
+        var surname = ""
+        var result = ""
+        for i in fullName.reversed() {
+            if i.isUppercase, let index = fullName.firstIndex(of: i)  {
+                surname = String(fullName.suffix(from: index))
+                name = String(fullName.prefix(upTo: index))
+                result = name + " " + surname
+                break
+            }
+        }
+        print("Имя-",name,"Фамилия-",surname,"Имя с фамилией-", result)
+    }
+    
+//-----
+    
+    
 
 
 }
